7b8e83a5cdbfed3eb2716bb6ac1f18522806db60
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-13 20:36:44 +0100

    chart.js

---
+++ src/components/defaults/Chart.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import Chart from "chart.js/auto";
+import { FaCalendarAlt, FaCaretLeft, FaCaretRight } from "react-icons/fa";
+
+function Graph(): JSX.Element {
+  useEffect(() => {
+    const canvas = document.getElementById("myChart") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      throw new Error("Unable to get 2D context for canvas");
+    }
+
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Mar", "Apr", "May", "Jun", "Jul", "Aug"],
+        datasets: [
+          {
+            data: [50, 150, 300, 500, 750, 1100],
+            label: "Users",
+            borderColor: "#3A5743",
+            backgroundColor: "#3A5743",
+          },
+        ],
+      },
+    });
+
+    return () => {
+      myChart.destroy();
+    };
+  }, []);
+
+  return (
+    <div className={`flex flex-col`}>
+      <div className="mb-10 flex justify-between bg-[#fff] rounded-lg py-2 px-4">
+        <h2>Users on Rehobothz</h2>
+        <div className="flex items-center space-x-3">
+          <FaCalendarAlt color="#808080" />
+          <span className="text-[13px] text-[#808080]">Last 6 months</span>
+          <FaCaretLeft />
+          <FaCaretRight />
+        </div>
+      </div>
+      {/* line chart */}
+      <div className="bg-white mx-4 px-4 pb-[3vh] pt-2">
+        <div className="">
+          <canvas id="myChart"></canvas>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Graph;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,19 @@
-import MainContainer from "../../components/containers/MainContainer"
-import Cards from "../../components/defaults/Cards"
+import MainContainer from "../../components/containers/MainContainer";
+import Cards from "../../components/defaults/Cards";
+import Graph from "../../components/defaults/Chart";
 
 const Dashboard = () => {
   return (
     <MainContainer active="dashboard">
-        <div className="mx-10">
-          <Cards />
-        </div>
-    </MainContainer>
-  )
-}
+      <div className="mx-10">
+        <Cards />
 
-export default Dashboard
+        <div className="w-[50%] mt-10">
+          <Graph />
+        </div>
+      </div>
+    </MainContainer>
+  );
+};
+
+export default Dashboard;
