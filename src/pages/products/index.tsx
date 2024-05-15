3684a342486c31cfca28fbb2ad333c5ab273402b
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-15 23:24:38 +0100

    basic logic to create new product

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,11 +1,22 @@
-import MainContainer from "../../components/containers/MainContainer"
+import MainContainer from "../../components/containers/MainContainer";
 
+const createProduct = async () => {
+  await fetch("http://localhost:4000/create-product", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 const ProductsCatalogue = () => {
   return (
     <MainContainer active="products">
-        <h2>Products Catalogue</h2>
+      <h2>Products Catalogue</h2>
+      <button className="create-produc" onClick={createProduct}>
+        Create Product
+      </button>
     </MainContainer>
-  )
-}
+  );
+};
 
-export default ProductsCatalogue
+export default ProductsCatalogue;
