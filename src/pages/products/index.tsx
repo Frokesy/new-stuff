de74bb15ea239e4abdf76b9bb953f719c79d4414
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-16 23:32:25 +0100

    perfected logic to create new product

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import MainContainer from "../../components/containers/MainContainer";
 
-const createProduct = async () => {
-  await fetch("http://localhost:4000/create-product", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+const ProductsCatalogue = () => {
+  const [data, setData] = useState({
+    name: "Test Data",
+    active: true,
+    description: "Test Description",
+    default_price_data: {
+      currency: "USD",
+      unit_amount: 5000,
     },
   });
-};
-const ProductsCatalogue = () => {
+  const createProduct = async () => {
+    await fetch("http://localhost:4000/create-product", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ items: data }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
   return (
     <MainContainer active="products">
       <h2>Products Catalogue</h2>
