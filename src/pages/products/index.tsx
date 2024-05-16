f76ff645048a63d3af9af642f7360b6b6e1de55e
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-16 23:58:44 +0100

    wrote logic to fetch all products from stripe

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MainContainer from "../../components/containers/MainContainer";
 
 const ProductsCatalogue = () => {
@@ -27,10 +27,29 @@ const ProductsCatalogue = () => {
       });
   };
 
+  const fetchAllProducts = async () => {
+    await fetch("http://localhost:4000/fetch-products", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
+  useEffect(() => {
+    fetchAllProducts()
+  }, [])
+
   return (
     <MainContainer active="products">
       <h2>Products Catalogue</h2>
-      <button className="create-produc" onClick={createProduct}>
+      <button className="create-product" onClick={createProduct}>
         Create Product
       </button>
     </MainContainer>
